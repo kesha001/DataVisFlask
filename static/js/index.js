04ce5813ad9e1c25cd27624d438c4e528beb28cb
Danylo Berdnyk <berdnykdanylo001@gmail.com>
2024-11-01 20:48:48 +0200

    js people creation

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,8 @@
 import { DebugForm } from "./debug.js";
+import { People } from "./people.js";
 
 function main() {
+  new People();
   if (document.querySelector(".debug-card")) {
     const debug = new DebugForm();
     debug.showResponse("");
