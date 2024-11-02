b7c0c79a48f07816a1d7f0ab3b93bf0eea002447
Danylo Berdnyk <berdnykdanylo001@gmail.com>
2024-11-02 17:51:14 +0200

    notes html/css + js functionality

--- static/js/request.js
+++ static/js/request.js
@@ -9,19 +9,19 @@ export function getData(endpoint, callback) {
     request.send();
   }
 
-  export function sendForm(form, action, endpoint, callback) {
-    const formData = new FormData(form);
-    const dataJSON = JSON.stringify(Object.fromEntries(formData));
+export function sendForm(form, action, endpoint, callback) {
+  const formData = new FormData(form);
+  const dataJSON = JSON.stringify(Object.fromEntries(formData));
 
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-      if (request.readyState === 4) {
-        callback(request.response, form);
-      }
-    };
-    request.open(action, endpoint);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(dataJSON);
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      callback(request.response, form);
+    }
+  };
+  request.open(action, endpoint);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(dataJSON);
 }
 
       
