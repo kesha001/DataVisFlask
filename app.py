38604ab3d200686644528480e26445901d66440e
Danylo Berdnyk <berdnykdanylo001@gmail.com>
2024-11-09 17:02:29 +0200

    basic pagination

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, url_for
 import config
 from models import Person
 
@@ -9,7 +9,14 @@ def home():
     # people = Person.query.all()
     page = request.args.get('page', 1, type=int)
     people = Person.query.paginate(page=page, per_page=config.PEOPLE_PER_PAGE, error_out=False)
-    return render_template("home.html", people=people.items)
+
+    next_url = url_for('home', page=people.next_num) if people.has_next else None
+    prev_url = url_for('home', page=people.prev_num) if people.has_prev else None
+
+    return render_template("home.html", people=people.items,
+                           next_url=next_url,
+                           prev_url=prev_url
+                        )
 
 
 
